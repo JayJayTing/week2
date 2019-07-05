5a4bcfad658af063e732ad46ac47a80b47f0805c
JayJayTing <tingjayj@gmail.com>
2019-07-04 21:38:12 -0400

    done

---
+++ d4-api/iss_spotter/iss_promised.js
@@ -0,0 +1,40 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+    const ip = JSON.parse(body).ip;
+    return request(`https://ipvigilante.com/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body){
+    const {latitude, longitude} = JSON.parse(body).data
+    const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+    console.log(url);
+  return request(url);
+}
+
+const nextISSTimesForMyLocation = function(){
+    return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    })
+
+    .catch((error)=> console.log(error));
+}
+
+
+
+module.exports = { nextISSTimesForMyLocation };
+
